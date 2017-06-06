87f6f72254e59fb021ca39f14650662d9e32738c
Shubham Aggarwal <aggarwals5411@gmail.com>
2017-06-07 02:34:06 +0530

    New Section added and simple input (read) added

---
+++ inputMethods/input.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "$ What is your name"
+read name
+echo "Enter your credentials"
+read -p 'Username: ' username
+read -sp 'Password: ' pass
+echo "Hey $name. Your username is $username and your password is $pass"
+
+ 
